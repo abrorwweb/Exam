414d087bcb6319c8028d85042f391b4c48ef0c3b
abrorjon0990 <abdurahimovabrorjon346@gmail.com>
2025-03-18 22:53:38 +0300

    struktura

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import {Home, Contact, About} from "./pages"
+import MainLayout from "./layouts/MainLayout"
+function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: "/about",
+          element: <About />
+        },
+        {
+          path: "/contact",
+          element: <Contact />
+        },
+      ],
+    },
+  ])
+  return <RouterProvider  router={routes}/>
+}
+
+export default App
+
