b817036947c0c02f102df724675b06b31d52663e
abrorjon0990 <abdurahimovabrorjon346@gmail.com>
2025-03-25 17:08:52 +0300

    google sign

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,16 +3,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD59IWqBeVxu_ryBZdS82l4an9WihLlx1I",
-  authDomain: "my-splash-60734.firebaseapp.com",
-  projectId: "my-splash-60734",
-  storageBucket: "my-splash-60734.appspot.com",
-  messagingSenderId: "748108951989",
-  appId: "1:748108951989:web:b7de8d1d654f8587db29fc",
+  apiKey: "AIzaSyARzTyw3BeKKcWOk5pL0txfeNrjIeKx3jM",
+  authDomain: "my-unsplash-8c128.firebaseapp.com",
+  projectId: "my-unsplash-8c128",
+  storageBucket: "my-unsplash-8c128.firebasestorage.app",
+  messagingSenderId: "1075685464201",
+  appId: "1:1075685464201:web:3f72765002ccbb0cd3ce58"
 };
 
 const app = initializeApp(firebaseConfig);
 
-// auth
 export const auth = getAuth();
 export const db = getFirestore(app);
+
+
